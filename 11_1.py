8360e56518f01596a66bbc87a74f1c74a043e094
dets30000 <mhorbal@outlook.com>
2018-06-03 10:43:31 -0400

    Update 11_1.py

--- 11_1.py
+++ 11_1.py
@@ -1,12 +1,35 @@
 import re
+
 name=input("Enter file: ")
-if len(name)<1: name="regex_sum_42.txt"
+
+if len(name)<1: name="regex_sum_99190.txt"
+
 handle=open(name)
 
+ 
+
 numbers=list()
-y=list()
+
+numint=list()
+
+ 
 
 for line in handle:
+
     line=line.rstrip()
-    numbers=re.findall('[0-9+]',line)
-    
+
+    numbers=re.findall('[0-9]+',line)
+
+    if len(numbers) < 1: continue
+
+    numbers = [int(x) for x in numbers]
+
+    for i in numbers:
+
+        numint.append(i)
+
+ 
+
+print(sum(numint))
+
+ 
